8e041cd19e9bc6edc9a2c22bfb5e079347689bca
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-05-19 19:44:09 +0300

    9- Dikey ScrolView

--- App.tsx
+++ App.tsx
@@ -1,64 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
-
-  const { height, width } = Dimensions.get("window")
-
-  const  showAlert =()=>{
-    Alert.alert(
-      "Bilgi",
-      "Bu Bir Bilgi",
-      [
-        {text:'iptal'},
-        {text:'Devam Et', onPress:()=>console.log('Devam Et Tıklandı')}
-      ]
-    )
-  }
-
   return (
-    <>
-      <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
+      {Array.from({ length: 30 }).map((_, i) => (
+        <View key={i} style={styles.item}>
+          <Text> Öge {i+1}</Text>
+        </View>
+      ))}
 
-       <Button title='Uyarı Göster' onPress={showAlert}/>
-
-
-      </View>
-
-
-    </>
-
-  );
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 16,
+    alignItems: 'center'
   },
-  title: {
-    color: '#003AFF',
-    fontWeight: "900",
-    fontSize: 36
-  },
-  image: {
-    width: 400,
-    height: 200,
-    borderRadius: 20
-  },
-  text:{
-    color:"#fff",
-    fontSize:20,
-    fontWeight:"700",
-    paddingHorizontal:8,
-    paddingVertical:4
-
-  },
-  bg:{
-  
-    justifyContent:'center',
-    alignItems:'center'
+  item:{
+    width:'90%',
+    padding:20,
+    marginBottom:12,
+    backgroundColor:'#e0f7fa',
+    borderRadius:12
   }
-});
+})
