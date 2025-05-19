217f78112e360dec6c0b6aed5a41ba30e51c7628
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-05-19 19:48:40 +0300

    10- Yatay ScrolView

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export default function App() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.container}>
       {Array.from({ length: 30 }).map((_, i) => (
-        <View key={i} style={styles.item}>
-          <Text> Öge {i+1}</Text>
+        <View key={i} style={styles.box}>
+          <Text style={styles.letter}> Öge {i + 1}</Text>
         </View>
       ))}
 
@@ -17,13 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 16,
-    alignItems: 'center'
+    marginTop:50
   },
-  item:{
-    width:'90%',
-    padding:20,
-    marginBottom:12,
-    backgroundColor:'#e0f7fa',
-    borderRadius:12
+  box: {
+    width: 100,
+    height: 100,
+    marginRight: 12,
+    backgroundColor: '#ffe0b2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12
+
+  },
+  letter: {
+    fontSize: 24,
+    fontWeight: "800",
+    color: '#e65100',
+
   }
 })
