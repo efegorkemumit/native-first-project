263b27e46ab5c8c3294bbb7dfb5447e45685e604
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2025-05-19 06:27:14 +0300

    5- Dimensions kullanımı Responsive Tasarım oluşturma

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const {height, width} = Dimensions.get("window")
   return (
     <>
       <View style={styles.container}>
         <Text style={{color:"#007AFF"}}>Open up App.tsx to start working on your app!</Text>
         <Text style={styles.title}>Open up App.tsx to start working on your app!</Text>
+        <Text>Ekran Genişliği: {Math.round(width)} px</Text>
+      <Text>Ekran Yüksekliği: {Math.round(height)} px</Text>
+
+      <Text style={{borderWidth:2, width:width*0.9, height:height*0.2, fontSize:width>400 ? 24:16}}>Test</Text>
+
 
         <StatusBar style="auto" />
       </View>
